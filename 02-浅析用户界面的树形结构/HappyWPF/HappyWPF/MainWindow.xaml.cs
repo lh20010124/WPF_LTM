16ef7610bb11213a17083111a5c35a8f068daca3
heheli <lh20010124@163.com>
2025-07-17 13:18:14 +0800

    202507171318_GongSi

---
+++ 02-浅析用户界面的树形结构/HappyWPF/HappyWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HappyWPF;
+
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        Human h = this.FindResource("human") as Human;
+        if (h!=null)
+        {
+            MessageBox.Show(h.Child.Name);
+        }
+    }
+}
+
+[TypeConverterAttribute(typeof(NameToHumanTypeConverter))]
+public class Human
+{
+    public string Name { get; set; }
+    public Human Child { get; set; }
+}
+
+public class NameToHumanTypeConverter:TypeConverter
+{
+    public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+    {
+        string name = value.ToString();
+        Human child = new Human();
+        child.Name = name;
+        return child;
+    }
+}
