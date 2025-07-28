dd58f69c05ad44b86dc63bfc28626a560aa758de
heheli <lh20010124@163.com>
2025-07-28 15:49:59 +0800

    202507281550_GongSi

---
+++ 04-x名称空间及其常用元素/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        stu1.ID = 1111;
+    }
+
+    private void button1_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show(stu1.ID.ToString());
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        WindowHappy wh = new WindowHappy();
+        wh.textBox1.Text = "I'm happy";
+        wh.ShowDialog();
+    }
+}
+public class Student
+{
+    public int ID { get; set; }
+}
